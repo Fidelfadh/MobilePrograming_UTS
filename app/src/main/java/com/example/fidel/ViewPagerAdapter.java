8ed61ee5d2b1dd82bb62e9f77cf11cc5a35a1cf1
Fidelfadh <fidelfadh21@gmail.com>
2022-10-31 07:52:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fidel/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.fidel;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.fidel.fragment.fragment1;
+import com.example.fidel.fragment.fragment2;
+import com.example.fidel.fragment.fragment3;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity){
+        super(fragmentActivity);
+    }
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new fragment1();
+            case 1:
+                return new fragment2();
+            case 2:
+                return new fragment3();
+            default:
+                return new fragment1();
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/fidel/fragment/fragment2.java
@@ -0,0 +1,24 @@
+package com.example.fidel.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.example.fidel.R;
+
+import java.util.ArrayList;
+
+
+public class fragment2 extends Fragment {
+
+    ListView listView;
+    String mTitle[]={"Sepeda Lipat", "Sepeda Gunung", "Sepeda BMX", "Sepeda Listrik Model 1", "Sepeda Listrik Model 2"};
+
+
+
+
+}
+
+
+
+
